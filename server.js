8ab1097d9ef14b40642f032464664320bfa0ada0
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2024-09-27 19:29:35 -0300

    feat(t1): obtem coords a partir de cidade

--- server.js
+++ server.js
@@ -7,15 +7,19 @@ const app = express();
 app.use(express.json());
 
 const {
-    SERVER_PORT, PROTOCOL, BASE_URL_COORDS, API_KEY, LANG_API, LIMIT_COORDS
-} = process.env
+    SERVER_PORT, PROTOCOL, BASE_URL_COORDS, BASE_URL_WEATHER, API_KEY, WEATHER_UNITS, LANG_API, LIMIT_COORDS
+} = process.env;
 
 app.route('/coords')
     .get(async (req, res) => {
         const cidade = req.query.cidade;
 
         if (!cidade) {
-            return res.status(400).json({ error: 'O nome da cidade é obrigatório' });
+            return res.status(400).send(`
+              <h1>400 Bad Request</h1>
+              <p>O nome da cidade é obrigatório</p>
+              <img src="https://http.cat/400" alt="400 Bad Request">
+            `);
         }
 
         try {
@@ -26,20 +30,28 @@ app.route('/coords')
                 const { lat, lon } = response.data[0];
                 return res.json({ cidade, latitude: lat, longitude: lon });
             } else {
-                return res.status(404).json({ error: `Nenhuma coordenada encontrada para a cidade: ${cidade}` });
+                return res.status(404).send(`
+                  <h1>404 Not Found</h1>
+                  <p>Nenhuma coordenada encontrada para a cidade: ${cidade}</p>
+                  <img src="https://http.cat/404" alt="404 Not Found">
+                `);
             }
         } catch (error) {
             console.error(`Erro ao buscar coordenadas: ${error}`);
-            return res.status(500).json({ error: 'Erro ao buscar coordenadas' });
+            return res.status(500).send(`
+              <h1>500 Internal Server Error</h1>
+              <p>Erro ao buscar coordenadas</p>
+              <img src="https://http.cat/500" alt="500 Internal Server Error">
+            `);
         }
     })
     .all((req, res) => {
         res.status(405).send(`
-      <h1>405 Method Not Allowed</h1>
-      <img src="https://http.cat/405" alt="405 Method Not Allowed">
-    `);
-    })
-;
+          <h1>405 Method Not Allowed</h1>
+          <img src="https://http.cat/405" alt="405 Method Not Allowed">
+        `);
+    });
+
 
 app.use((req, res) => {
     res.status(404).send(`
@@ -52,10 +64,11 @@ app.use((err, req, res, next) => {
     const statusCode = err.status || 500;
     res.status(statusCode).send(`
       <h1>${statusCode} Error</h1>
+      <p>${err.message || 'Ocorreu um erro inesperado.'}</p>
       <img src="https://http.cat/${statusCode}" alt="${statusCode} Error">
     `);
 });
 
-app.listen(SERVER_PORT, () => {
-    console.log(`Servidor rodando na porta ${SERVER_PORT}`);
+app.listen(SERVER_PORT || 3000, () => {
+    console.log(`Servidor rodando na porta ${SERVER_PORT || 3000}`);
 });
